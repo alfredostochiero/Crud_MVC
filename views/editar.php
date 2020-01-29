f4cf5debe98a064a9ec384dd240e4976e28b6c2b
Alfredo Stochiero <alfredo_stochiero@hotmail.com>
2020-01-29 13:15:34 -0300

    Place holders mostram o nome antigo

--- views/editar.php
+++ views/editar.php
@@ -4,11 +4,12 @@
 
 <?php 
 $id = $_GET['id'];
+$nomeAntigo = $_GET['nome'];
 ?>	
 <form method="POST" action="<?php echo BASE_URL;?>contatos/editarContatos">
 	<div class="form-group">
-		<label for="nome"> Nome: </label>
-		<input type="text" class="form-control"  id="nome" placeholder="Digite seu nome completo" name="nome"/>
+		<label for="nome">Novo nome: </label>
+		<input type="text" class="form-control"  id="nome" placeholder="<?php echo "Nome atual : ".$nomeAntigo ?>" name="nome"/>
 	</div>
 	<input type="submit" value ="Editar" class="btn btn-primary"/>
 	<input type="hidden" name="id" value="<?php echo $id?>"> 
